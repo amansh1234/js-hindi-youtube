06ccd02fc3667ec7fe68f2bb5ad8cd8cf4970f16
amansh1234 <93636545+amansh1234@users.noreply.github.com>
2023-08-23 10:26:57 +0000

    Variables and datatypes

---
+++ .devcontainer/01_basics/01_variables
@@ -0,0 +1,13 @@
+const accountID = 144553
+let accountEmail ="amanchauhan732932gmail.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+let accountState;
+//accountID = 2// not allowed
+/*prefer not to use var in java script because of issue in block scope and functioal scope*/
+accountEmail= "ha@ha1gmail.com"
+accountPassword= "123"
+accountCity= "moradabad"
+
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat  all js code as a newer version of javascript
+
+//alert (3+3) we are using node js not browser
+
+console.log(3+3);// always starts with a new line because code redebolity should be high
+
+let name ="Aman"// string
+let age = "21" // number
+let isloggedIn = false // or can be true Boolean type
+
+// number => 2 ti the power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null =>standalone value(empty not undefined)
+//undefined=> 
+//symbol=> uniqe
+
+
+//object
+
+console.log(typeof age)// know the type
