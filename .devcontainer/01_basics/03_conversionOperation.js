e73d727a6485890ba15912984aba38e3679b4723
amansh1234 <93636545+amansh1234@users.noreply.github.com>
2023-08-24 04:05:59 +0000

    Conversion of Data types

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score =undefined;
+
+//console.log (typeof score);
+
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+// "33" => 33 
+//"33abc" => NaN => Not an number
+
+//true = 1; false = 0
+let isLoggedIn= ""
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//""=> false
+// "aman"=> true
+
+let somenumber =33
+let stringnumber= String(somenumber)
+console.log(typeof stringnumber);
