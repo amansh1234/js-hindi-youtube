93344a4099ce60ea9843b74044f3601cab820c2e
amansh1234 <93636545+amansh1234@users.noreply.github.com>
2023-08-28 05:32:53 +0000

    Addition

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -13,3 +13,24 @@ const id= Symbol('123')
 const anotherId = Symbol('123')
 
 console.log(id==anotherId);
+
+//++++++++++++++++
+// stack(primitive) , heap(non primitive)
+// example
+
+let myyoutubename= "Amanchauhancom"
+
+let anothername = myyoutubename
+anothername = "chaiorcode"
+console.log(myyoutubename);
+console.log(anothername);
+
+// heap example
+// In heap value is changed but in stack value is not changed in stack we only get copy of the variable 
+let Aman ={
+email: "aman@gmail.com"
+}
+let puranprakash =Aman
+Aman.email ="rohan@gmail.com"
+console.log(Aman);
+console.log(puranprakash);
